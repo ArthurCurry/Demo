dc2684f4d1ce8fbbdaabc7228f9a6689ada33875
wxj <825754896@qq.com>
2019-03-07 22:48:28 +0800

    关卡变动，机关和新脚本

---
+++ Assets/Scripts/Mechanism/Lift.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lift : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject active;
+    [SerializeField]
+    private GameObject passive;
+    [SerializeField]
+    private bool triggered;
+
+	// Use this for initialization
+	void Start () {
+        triggered = false;
+	}
+	
+    void LateUpdate()
+    {
+        if(!triggered)
+        {
+            if((active.transform.position-passive.transform.position).magnitude<0.1f)
+            {
+                triggered = true;
+                LiftUp();
+            }
+        }
+    }
+
+    void LiftUp()
+    {
+        foreach(Transform child in this.GetComponentInChildren<Transform>())
+        {
+            if(!transform.name.Equals(this.transform.name))
+            {
+                child.GetComponent<BoxCollider2D>().enabled = true;
+                child.GetComponent<SpriteRenderer>().color = Color.white;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Monster/AttackMode1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackMode1 : MonoBehaviour {
+
+    private GameObject player;
+    private Rigidbody2D playerRB;
+    private PlayerMovements pm;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag(HashID.PLAYER);
+        playerRB = player.GetComponent<Rigidbody2D>();
+        pm = player.GetComponent<PlayerMovements>();
+	}
+	
+
+    void LateUpdate()
+    {
+        if(playerRB.velocity==Vector2.zero)
+        {
+            if ((player.transform.position - this.transform.position).magnitude < 0.1f)
+                pm.isDead = true;
+        }
+    }
+}
