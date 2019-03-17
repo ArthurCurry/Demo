8dcca1fe37de6ad0a5d8e78c1bc45c928ae4dadd
wxj <825754896@qq.com>
2019-03-17 16:31:44 +0800

    背包，拼图管理

--- Assets/Scripts/Mechanism/Trigger.cs
+++ Assets/Scripts/Mechanism/Trigger.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class Trigger : MonoBehaviour {
     private Transform player;
+    private bool dont;
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindWithTag(HashID.PLAYER).transform;
+        dont = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(this.transform .position .Equals (player .position))
+		if(this.transform .position .Equals (player .position)&&dont)
         {
             BuildManager.IsCG = true;
             BuildManager.GetCount(BuildManager.Name);
             BuildManager.InitDialog();
+            dont = false;
         }
 	}
 }
