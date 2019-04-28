b2b4945965ed7c273deb7a9bef0b430a622b484d
AoqiWang <1017802402@qq.com>
2019-04-28 21:03:58 +0800

    游戏介绍的加入

---
+++ Assets/Scripts/MVC/Ctrls/IntroductionCtrl.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+
+public class IntroductionCtrl : MonoBehaviour
+{
+
+    public GameObject introductionText;
+    public GameObject introTitle;
+    public GameObject introSprite;
+    public int page = 1;
+    Introduction introduction = null;
+    bool Changed = true;
+
+	void Start () {
+        
+        //IntroductionModel.PageList.TryGetValue(page, out introduction);
+        
+	}
+    public string GetIntroductionText(Introduction introduction)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendFormat("<color=black><size=30>游戏介绍：{0}</size></color>",introduction.IntroText);
+        introductionText.GetComponent<Text>().text = introduction.IntroText;
+        return sb.ToString();
+       
+    }
+
+    public void GetIntroductionSprite(Introduction introduction)
+    {
+        //Debug.Log(introduction.ImageIcon);
+        Sprite sp = Resources.Load<Sprite>("Prefabs/Introduction/"+introduction.ImageIcon);
+        introSprite.GetComponent<Image>().sprite = sp;
+    }
+
+    public string GetIntroductionTitle(Introduction introduction)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendFormat("<color=black><size=50>{0}</size></color>", introduction.Title);
+        introTitle.GetComponent<Text>().text = introduction.Title;
+        return sb.ToString();
+
+    }
+
+    private void Update()
+    {
+        if(Changed == true)
+        {
+            IntroductionModel.PageList.TryGetValue(page, out introduction);
+            Debug.Log(1);
+             GetIntroductionSprite(introduction);
+            Debug.Log(2);
+            introductionText.GetComponent<Text>().text = GetIntroductionText(introduction);
+            introTitle.GetComponent<Text>().text = GetIntroductionTitle(introduction);
+            Changed = false;
+        }
+    }
+    public void AddPage()
+    {
+        if (page == 8) return;
+        else
+        {
+            page++;
+            Changed = true;
+        }       
+    }
+
+    public void DecresePage()
+    {
+        if (page == 1) return;
+        else
+        {
+            page--;
+            Changed = true;
+        }       
+    }
+}
---
+++ Assets/Scripts/MVC/Models/Introduction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introduction {
+
+    public string Title { get; private set; }
+    public string IntroText { get; private set; }
+    public string ImageIcon { get; private set; }
+
+    public Introduction(string title,string introText,string imageIcon)
+    {
+        this.Title = title;
+        this.IntroText = introText;
+        this.ImageIcon = imageIcon;
+    }
+}
