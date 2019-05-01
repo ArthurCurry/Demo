1b0d8c1321231336f73c1c499a55b1d2ad7b0943
AoqiWang <1017802402@qq.com>
2019-05-01 17:17:55 +0800

    游戏介绍面板背景字体消逝，收藏室的初步设置

---
+++ Assets/Scripts/CGCollection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Sprites;
+using UnityEngine.EventSystems;
+
+public class CGCollection : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public int CGNumber;
+    public bool isWatched = false;
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (isWatched == true)
+        {
+            Sprite sp = Resources.Load<Sprite>("Materials/CG/CG" + CGNumber);
+            this.GetComponent<Image>().sprite = sp;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log(CGNumber);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log(CGNumber + 100);
+    }
+}
